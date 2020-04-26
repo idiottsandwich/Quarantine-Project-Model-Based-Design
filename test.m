5dea45f83ef18a2080696046f241a2c685ff5fe4
idiottsandwich <clin415@bu.edu>
2020-04-26 16:04:38 -0400

    yah

--- test.m
+++ test.m
@@ -22,7 +22,7 @@
 % ANY WAY OUT OF THE USE OF THIS
 % SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 %-------------------------------------------------------------------------
-
+% yo
 % this code uses tf (transfer function) from the control system toolbox to
 % model the step response of a FSAE car front and rear suspension
 num = 1;
